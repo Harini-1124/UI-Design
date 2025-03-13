fd448e2e2a6006aec9f276f1c2718d9a6f838f1b
Harini-1124 <harinisrinivas112@gmail.com>
2025-03-13 12:53:41 +0530

    Add files via upload

---
+++ LAB/switchpackage.dart
@@ -0,0 +1,14 @@
+import 'package:switchpackage/switchpackage.dart';
+import 'package:switchpackage/switchvar.dart'
+import 'dart:io';
+
+void main(List<String> arguments) {
+	switchpackage sp = switchpackage();
+	print("Enter color: ");
+	String color = stdin.readLineSync()!;
+	print(sp.checkcolor("color"));
+	print("Enter season: ");
+	String season = stdin.readLineSync()!;
+	switchvar = switchvar();
+	sv.checkMonths(season);
+}
